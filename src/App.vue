6de2b16794b166b4584f8cf6adddf341c7aba85d
Dmytro Petrenko <dmitry.a.petrenko@gmail.com>
2021-08-02 17:32:02 +0300

    first init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<ul>
+			<li :key="user.id" v-for="user in users">{{ user.firstName }}</li>
+		</ul>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: "App",
+	data() {
+		return {
+			users: [
+				{
+					id: 1,
+					firstName: "Dmytro",
+					secondName: "Petrenko",
+					position: "Trainee Vue Js Developer",
+				},
+				{
+					id: 2,
+					firstName: "Valeriy",
+					secondName: "Nosenko",
+					position: "Trainee React Js Developer",
+				},
+				{
+					id: 3,
+					firstName: "Andriy",
+					secondName: "Parkhomenko",
+					position: "Senior Vue Js Developer",
+				},
+				{
+					id: 4,
+					firstName: "Artemon",
+					secondName: "Bulangery",
+					position: "Designer",
+				},
+			],
+		}
+	},
+	components: {},
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
